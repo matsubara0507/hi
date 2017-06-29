6ca4ae1d148ef94f703279720921144d0905125c
松原信忠 <t12307043@gunma-u.ac.jp>
2017-06-29 23:05:44 +0900

    Created hi
    but implementation has not been done yet.

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,40 @@
-module Main
-    ( main
-    , File
-    , editFile
-    ) where
+module Main where
 
 main :: IO ()
 main = putStrLn "Hello, World!"
 
-type File = [String]
+type Args = [String]
 
-editFile :: File -> File
-editFile = id
+hi :: Args -> IO ()
+hi args = do
+  es1 <- mkInitEState args
+  print es1
+  cmd <- readCmd
+  let es2 = edit cmd es1
+  print es2
+
+type File = [String]
+type Cursor = (Int, Int)
+
+data EState = EState
+            { getFilePath :: FilePath
+            , getFileContents :: File
+            , getCursor :: Cursor
+            } deriving (Show)
+
+data Cmd = Insert String
+         | UpCursor
+         | DownCursor
+         | RightCursor
+         | LeftCursor
+         | Delete
+         deriving (Show)
+
+mkInitEState :: Args -> IO EState
+mkInitEState = undefined
+
+readCmd :: IO Cmd
+readCmd = undefined
+
+edit :: Cmd -> EState -> EState
+edit = undefined
