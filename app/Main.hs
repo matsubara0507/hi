f74352de7c626eec6e00424427b78728403501ab
松原信忠 <t12307043@gunma-u.ac.jp>
2017-06-29 22:09:17 +0900

    Added editFile that is id

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main
     ( main
+    , File
+    , editFile
     ) where
 
 main :: IO ()
 main = putStrLn "Hello, World!"
+
+type File = [String]
+
+editFile :: File -> File
+editFile = id
