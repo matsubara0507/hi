d1ebab1351557fc11f12f0473d94034a5820c1be
松原信忠 <t12307043@gunma-u.ac.jp>
2017-07-01 18:44:25 +0900

    Added function that edit file

--- app/Main.hs
+++ app/Main.hs
@@ -54,4 +54,34 @@ toCmd ('a' : ' ' : txt) = Insert txt
 toCmd s = error $ mconcat ["undefined command \"", s, "\""]
 
 edit :: Cmd -> EState -> EState
-edit = undefined
+edit cmd = updateFile cmd . moveCursor cmd
+
+updateFile :: Cmd -> EState -> EState
+updateFile (Insert txt) es@(EState _ fc (r,c)) = es { getFileContents = insertTextOnFile r c fc txt }
+updateFile Delete       es@(EState _ fc (r,c)) = es { getFileContents = deleteCharOnFile r c fc }
+updateFile _ es = es
+
+insertTextOnFile :: Int -> Int -> File -> String -> File
+insertTextOnFile 0 c (x:xs) txt = insertTextOnLine c x txt : xs
+insertTextOnFile n c (x:xs) txt = x : insertTextOnFile (n-1) c xs txt
+
+insertTextOnLine :: Int -> String -> String -> String
+insertTextOnLine 0 line txt = txt `mappend` line
+insertTextOnLine n (x:xs) txt = x : insertTextOnLine (n-1) xs txt
+
+deleteCharOnFile :: Int -> Int -> File -> File
+deleteCharOnFile 0 c (x:xs) = deleteCharOnLine c x : xs
+deleteCharOnFile n c (x:xs) = x : deleteCharOnFile (n-1) c xs
+
+deleteCharOnLine :: Int -> String -> String
+deleteCharOnLine 0 (x:xs) = xs
+deleteCharOnLine n (x:xs) = x : deleteCharOnLine (n-1) xs
+
+moveCursor :: Cmd -> EState -> EState
+moveCursor cmd es@(EState _ fc csr) = es { getCursor = moveCursor' cmd csr}
+  where
+    moveCursor' UpCursor    (r,c) = (r - (if r > 0 then 1 else 0), c)
+    moveCursor' DownCursor  (r,c) = (r + (if r < length fc - 1 then 1 else 0), c)
+    moveCursor' LeftCursor  (r,c) = (r, c - (if c > 0 then 1 else 0))
+    moveCursor' RightCursor (r,c) = (r, c + (if c < length (fc !! r) - 1 then 1 else 0))
+    moveCursor' _ csr = csr
